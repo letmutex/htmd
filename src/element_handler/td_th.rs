e6cbda98d19f8ff9a184ac3d4319d933b08f94a2
Bryan A. Jones <bjones1@user.noreply.github.com>
2025-12-09 17:56:06 -0600

    Fix: in faithful mode, table cell must not contain block elements.

--- src/element_handler/td_th.rs
+++ src/element_handler/td_th.rs
@@ -1,11 +1,27 @@
 use crate::{
     Element,
-    element_handler::element_util::handle_or_serialize_by_parent,
-    element_handler::{HandlerResult, Handlers},
+    dom_walker::is_block_element,
+    element_handler::{HandlerResult, Handlers, element_util::handle_or_serialize_by_parent},
+    node_util::get_node_tag_name,
+    options::TranslationMode,
     serialize_if_faithful,
 };
 
 pub(super) fn td_th_handler(handlers: &dyn Handlers, element: Element) -> Option<HandlerResult> {
-    serialize_if_faithful!(handlers, element, 0);
+    // Are any of this node's children block elements?
+    let has_block_elements = handlers.options().translation_mode == TranslationMode::Faithful
+        && element
+            .node
+            .children
+            .borrow()
+            .iter()
+            .any(|child| get_node_tag_name(child).is_some_and(is_block_element));
+    serialize_if_faithful!(
+        handlers,
+        element,
+        // Force HTML serialization in faithful mode if the table cell contains
+        // block elements.
+        if has_block_elements { -1 } else { 0 }
+    );
     handle_or_serialize_by_parent(handlers, &element, &vec!["tr"], true)
 }
